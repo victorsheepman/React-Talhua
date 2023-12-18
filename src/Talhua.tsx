4ffe15e5d2640ae423a721f2ba74ca5a70e614e8
victor_marquez <victorsheepman@gmail.com>
2023-12-18 16:11:38 -0400

    add redux toolkit

--- src/Talhua.tsx
+++ src/Talhua.tsx
@@ -1,5 +1,30 @@
+import { useAppDispatch, useAppSelector } from './toolkit/hooks'
+import { decrement, increment } from './toolkit/slices/counterSlice'
+
 function Talhua() {
-    return <div>hola</div>
+    const count = useAppSelector((state) => state.counter.value)
+    const dispatch = useAppDispatch()
+    return (
+        <div>
+            <div>
+                <button
+                    type="button"
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <span>{count}</span>
+                <button
+                    type="button"
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    Decrement
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default Talhua
