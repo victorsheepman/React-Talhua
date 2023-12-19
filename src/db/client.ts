5594c6afcd6d25a135c8e50555ae68cb5b6b8ae0
victor_marquez <victorsheepman@gmail.com>
2023-12-19 11:36:42 -0400

    progress

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-    'https://sqrevjfizglmjxxfuvxy.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxcmV2amZpemdsbWp4eGZ1dnh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI5MTE5MDcsImV4cCI6MjAxODQ4NzkwN30.-eI2fmeRlqgwcMT2CaAF1WSYzm9jIbCEP9E-CEVugmw'
-)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
